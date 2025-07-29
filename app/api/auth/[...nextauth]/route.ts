cbf838b8721503b96fbd46e60d41a0f200eb880d
albijamesdt <albin.james@digitrell.com>
2025-07-29 13:17:48 +0530

    fix isue in auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,6 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compare } from "bcryptjs";
 
-export const runtime = 'edge';
 export const authOptions = {
     
   providers: [
